540624ccafcca60d42399568e6087d4a4cece3e8
Polvere243 <danielercoli@gmail.com>
2024-10-25 12:24:36 +0200

    looped over slides and commented some css properties

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
+
+// faccio un ciclo sulle immagini
+
+slides.forEach((slide, index) => {
+
+});
