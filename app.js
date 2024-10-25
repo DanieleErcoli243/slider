175ba8d0bb135b5d590501ecfb39a6ce9db0633c
Polvere243 <danielercoli@gmail.com>
2024-10-25 12:30:26 +0200

    created a variable for the counter

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
+// creo un contatore
+let counter = 0;
 
 // faccio un ciclo sulle immagini
 
 slides.forEach((slide, index) => {
-
+    // manipolo lo stile delle slide
+    slide.style.left = `${index * 100}%`;
 });
+
